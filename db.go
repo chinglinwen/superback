8eb47380f9f63d066ed6ab2e86b7b3221228f902
Chinglin Wen <chinglinwen@gmail.com>
2016-12-17 08:07:36 +0800

    Remove commentted code

--- db.go
+++ db.go
@@ -17,7 +17,6 @@ type File struct {
 	//collectDate time.Time
 }
 
-//func (f *File) Read(p []byte) (n int, err error) {
 func (f *File) Read() (n int, err error) {
 	var value []byte
 	err = db.View(func(tx *bolt.Tx) error {
@@ -44,57 +43,3 @@ func (f *File) Write() (n int, err error) {
 	return buf.Len(), err
 }
 
-/*
-func (f *File) Read(p []byte) (n int, err error) {
-	err = db.View(func(tx *bolt.Tx) error {
-		value := tx.Bucket([]byte("files")).Get([]byte(f.Md5sum))
-		p=make([]byte,len(value))
-		copy(p, value)
-		return nil
-	})
-	return len(p), err
-}
-
-func (f *File) Write(p []byte) (n int, err error) {
-	err = db.Update(func(tx *bolt.Tx) error {
-		return tx.Bucket([]byte("files")).
-			Put([]byte(f.Md5sum), p)
-	})
-	return len(p), err
-}
-
-/*
-func (c *Cache) Set(key, value []byte) error {
-		err := c.db.Update(func(tx *bolt.Tx) error {
-			bucket, err := tx.CreateBucketIfNotExists(c.requestsBucket)
-			if err != nil {
-				return err
-			}
-			err = bucket.Put(key, value)
-			if err != nil {
-				return err
-			}
-			return nil
-		})
-
-		return err
-	}
-
-	func (c *Cache) Get(key []byte) (value []byte, err error) {
-	   err = c.db.View(func(tx *bolt.Tx) error {
-	      bucket := tx.Bucket(c.requestsBucket)
-	      if bucket == nil {
-	         return fmt.Errorf("Bucket %q not found!", c.requestsBucket)
-	      }
-
-	      var buffer bytes.Buffer
-	      buffer.Write(bucket.Get(key))
-
-	      value = buffer.Bytes()
-	      return nil
-	   })
-
-	   return
-	}
-
-*/
--- main.go
+++ main.go
@@ -53,50 +53,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//for {
-	//}
 }
 
-/*
-
-get all the file info
-
-store local
-have a ui for it,
-  fuzzy search?
-
-compare one by one, if no backup, backup then(upload to somewhere)
-can specify multiple path (using parameter)
-
-
-Remove the duplicate backup of all my files ( store only one in the cloud )
-To store the meta info in the cloud as a service
-
-Try run all things to collect that info
-
-Backup only one of them ( program do the backup )
-
-Focus on pdf first? (and other small file)
-
-
-Super backup
-Book index (multi dimension search, of just fuzzy search )
-Also provide a exact search
-
-Upload to one location may lost the organized info(that I spend long time on it)
-
-How to make it easier accessable
-To modify this meta info
-To easier download
-
-It may need a ui to do this
-
-Non pdf file out into other directory
-Will the file number exceed to max number in a directory?
-
-Backup to cloud and removable disk, tar from cloud, sync to local, how to do incremental sync
-
-There may have lots operation to list them all
-To sync the meta way, light way and heavy way (download to local first, upload the modified one?) So it involves meta update
-
-*/
